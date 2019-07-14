c0778c3d29c33295c2ccc50293c191bb136cf900
Mbuodile Obiosio <cazewonder@gmail.com>
2019-07-14 22:32:55 +0100

    Final

--- app/src/main/java/com/alc4obiosio/myprofile/AboutActivity.java
+++ app/src/main/java/com/alc4obiosio/myprofile/AboutActivity.java
@@ -8,6 +8,7 @@ import android.net.http.SslError;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.webkit.SslErrorHandler;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -66,6 +67,10 @@ public class AboutActivity extends AppCompatActivity {
             handler.proceed();
         }
 
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            return super.shouldOverrideUrlLoading(view, request);
+        }
 
         @Override
         public void onPageFinished(WebView view, String url) {
